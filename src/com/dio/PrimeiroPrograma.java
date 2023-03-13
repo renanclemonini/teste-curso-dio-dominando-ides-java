ada449fb47f20108265bb6665edb59ddcef6fc9f
renanzao <rclemon93@gmail.com>
2023-03-13 14:33:51 -0300

    new commit 2

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -7,11 +7,18 @@ public class PrimeiroPrograma {
 	
 	public static void main(String[] args) {
 		Gato gato = new Gato();
+		Livro livros = new Livro();
 		
 		System.out.println(gato);
+		System.out.println(livros);
 		
 		System.out.println("Deu certo?");
 		
 	}
 
+}
+
+class Livro {
+	public String titulo;
+	public String autor;
 }
