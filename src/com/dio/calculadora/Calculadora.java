38512bec7fd7aa7bb6d7750ff9843de17f6374dd
renanzao <rclemon93@gmail.com>
2023-03-13 16:59:20 -0300

    criação de calculadora

---
+++ src/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,49 @@
+package com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		double a, b;
+		
+		System.out.print("Digite um numero: ");
+		a = scan.nextDouble();
+		System.out.print("Digite outro numero: ");
+		b = scan.nextDouble();
+		
+		double soma = soma(a,b);
+		double subtrair = subtrair(a, b);
+		double multiplicar = multiplicar(a, b);
+		double dividir = dividir(a, b);
+		
+		System.out.println(a+" + "+b+" = "+soma);
+		System.out.println(a+" - "+b+" = "+subtrair);
+		System.out.println(a+" * "+b+" = "+multiplicar);
+		System.out.println(a+"/ "+b+" = "+dividir);
+		
+		
+		
+	}
+	
+	public static double soma(double a, double b) {
+		return a + b;
+	}
+	
+	public static double subtrair(double a, double b) {
+		return a - b;
+	}
+	
+	public static double dividir(double a, double b) {
+		return a / b;
+	}
+	
+	public static double multiplicar(double a, double b) {
+		return a * b;
+	}
+	
+	
+
+}
